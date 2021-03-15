641c70ebc764cec6678c97fb4003bc4a0760cb1e
Nihal Mohammed <mnihal64@outlook.com>
2021-03-15 19:28:52 +0530

    readem update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,26 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user!, only: %i[new create]
 
-    def index
-        @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def new
-        @post = Post.new
-    end
+  def new
+    @post = Post.new
+  end
 
-    def create
-        @post = current_user.posts.build(post_params)
-        if @post.save
-            redirect_to root_path
-        else
-            render :new
-        end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    private
-    
-    def post_params
-        params.require(:post).permit(:user_id, :title, :body)
-    end
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :title, :body)
+  end
 end

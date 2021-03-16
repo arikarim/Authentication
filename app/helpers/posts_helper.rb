c5aab653224c1ef2b33e3d005d32150476d676b3
Nihal Mohammed <mnihal64@outlook.com>
2021-03-16 11:48:12 +0530

    Move content logic to post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+    def post_info(post)
+        if user_signed_in?
+            content_tag(:div) do
+                content_tag(:p, post.user.email) +
+                content_tag(:p, post.title) +
+                content_tag(:p, post.body)
+            end
+        else
+            content_tag(:div) do 
+                content_tag(:p, post.title) +
+                content_tag(:p, post.body)
+            end
+        end 
+    end
+
+
 end

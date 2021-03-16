2aadd013bc5108e1cee0f5e6d3916749e778caa0
Nihal Mohammed <mnihal64@outlook.com>
2021-03-16 12:02:31 +0530

    Move user_sessions buttons to helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -14,5 +14,15 @@ module PostsHelper
         end 
     end
 
-
+    def user_sessions
+        if user_signed_in?
+            content_tag(:div) do
+                (button_to 'sign out', destroy_user_session_path, method: :delete) + 
+                (link_to 'new post', new_post_path, method: :delete)
+            end
+        else
+            (button_to 'sign in', new_user_session_path) +
+            (link_to 'sign up', new_user_registration_path) 
+        end
+    end
 end
